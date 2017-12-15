fc5a479121c6e60392395e6dc0fd945ca173eeed
axelmortimer <axelmortimer@gmail.com>
2017-12-15 17:55:49 -0500

    final take 2

--- js-index.js
+++ js-index.js
@@ -1,20 +1,27 @@
-var dailySeconds = 0;
-
 var fontSize = window.getComputedStyle(document.body).getPropertyValue('font-size');
 fontSize = fontSize.slice(0,2);
+var todayDateNumber = 0;
+
+var dailySecondsChart = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]];
+dailySecondsChart[6][0] = todayDate;
+dailySecondsChart[6][1] = dailySecondsArray[0][1];
+
+todayDateNumber = parseInt(dailySecondsChart[6][0].replace('/', ''));
+console.log(todayDateNumber);
+
 
 var linechartDaily = new ThreesyLine({
     element: "#chart-daily",
     height: 6.6875*fontSize,
     width: 16.25*fontSize,
     data: [
-        {x: "Mon", y: 1.2},
-        {x: "Tue", y: 0},
-        {x: "Wed", y: 0},
-        {x: "Thu", y: 0},
-        {x: "Fri", y: 0},
-        {x: "Sat", y: 0},
-        {x: "Sun", y: 0}
+        {x: 1, y: 1},
+        {x: 1, y: 1},
+        {x: 1, y: 1},
+        {x: 1, y: 1},
+        {x: 1, y: 1},
+        {x: 1, y: 1},
+        {x: 1, y: 1}
     ]
 });
 linechartDaily.draw();
@@ -34,3 +41,18 @@ var linechartWeekly = new ThreesyLine({
     ]
 });
 linechartWeekly.draw();
+
+// var linechartDaily = new ThreesyLine({
+//     element: "#chart-daily",
+//     height: 6.6875*fontSize,
+//     width: 16.25*fontSize,
+//     data: [
+//         {x: dailySecondsChart[0][0], y: dailySecondsChart[0][1]},
+//         {x: dailySecondsChart[1][0], y: dailySecondsChart[1][1]},
+//         {x: dailySecondsChart[2][0], y: dailySecondsChart[2][1]},
+//         {x: dailySecondsChart[3][0], y: dailySecondsChart[3][1]},
+//         {x: dailySecondsChart[4][0], y: dailySecondsChart[4][1]},
+//         {x: dailySecondsChart[5][0], y: dailySecondsChart[5][1]},
+//         {x: dailySecondsChart[6][0], y: dailySecondsChart[6][1]}
+//     ]
+// });
--- js-main.js
+++ js-main.js
@@ -1,43 +1,98 @@
 var historyTab = 0;
-
 var dailySeconds = 0;
+var dailyMinutes = 0;
+var dailyHours = 0.0;
+var dailySecondsArray = [];
+var dailySecondsArrayTemp = [];
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
 
+if(dd<10){
+    dd='0'+dd;
+}
+if(mm<10){
+    mm='0'+mm;
+}
+var todayDate = mm+'/'+dd;
+// Test Dates:
+// todayDate = '12/16';
+
+// Instantiate/Populate Daily Seconds Array
+if (typeof (Storage) !== "undefined") {
+
+  // Reset localStorage.dailySecondsArray for debugging
+  // localStorage.removeItem('dailySecondsArray');
+
+  // Sync LocalStorage to JS Variables
+  if (localStorage.dailySecondsArray) {
+    console.log('DailySecondsArray already exists.');
+    console.log('localStorage.dailySecondsArray: ', JSON.parse(localStorage.dailySecondsArray));
+
+    // Compare Dates
+    dailySecondsArrayTemp = JSON.parse(localStorage.dailySecondsArray);
+    if (dailySecondsArrayTemp[0][0] !== todayDate) {
+      // Add date to beginning of array
+      console.log('New day!');
+      dailySecondsArrayTemp.unshift([todayDate, 0, 0]);
+      console.log('dailySecondsArrayTemp: ', dailySecondsArrayTemp);
+      localStorage.dailySecondsArray = JSON.stringify(dailySecondsArrayTemp);
+    } else {
+      console.log('Same day!');
+    }
+
+  } else {
+    // Create Local Storage Daily Array If It Doesn't Exist
+    console.log('DailySecondsArray doesn\'t exist, creating new array.');
+    localStorage.dailySecondsArray = JSON.stringify([[todayDate, 0, 0]]);
+  }
+
+  // Match arrays
+  dailySecondsArray = JSON.parse(localStorage.dailySecondsArray);
+  console.log('dailySecondsArray: ', dailySecondsArray);
+
+} else {
+  // Sorry! No Web Storage support..
+  alert('This browser does NOT support local storage');
+}
+
+// Edit Total Hours
+document.getElementById('totalHours').innerHTML = JSON.stringify(dailySecondsArray[0][1]);
+
+// Change Page
 function jmp2LocalPage(whichPage) {
   location.href = whichPage;
 }
 
-function readData() {
-  console.log("readData called");
-  if (typeof (Storage) !== "undefined") {
-    dailySeconds = (parseInt(localStorage.dailySeconds) || 0);
-    console.log(dailySeconds);
-  } else {
-    // Sorry! No Web Storage support..
-    alert('This browser does NOT support local storage');
-  }
-}
-
-readData();
-
+// Save Seconds
 function saveTime() {
-  console.log("writeData called");
+  console.log("saveTime called");
   if (typeof (Storage) !== "undefined") {
-    localStorage.dailySeconds = (parseInt(localStorage.dailySeconds) || 0) + overallSeconds;
+    dailySecondsArray[0][2] = (dailySecondsArray[0][2] || 0) + overallSeconds;
+    dailySecondsArray[0][1] = computeHours(dailySecondsArray[0][2]);
+    localStorage.dailySecondsArray = JSON.stringify(dailySecondsArray);
   } else {
     // Sorry! No Web Storage support..
     alert('This browser does NOT support local storage');
   }
-  console.log(localStorage.dailySeconds);
+  console.log('Updated localStorage.dailySecondsArray: ', localStorage.dailySecondsArray);
   jmp2LocalPage('index.html');
 }
 
-function computeTime(seconds) {
-  
+function computeHours(seconds) {
+  dailyMinutes = seconds / 60;
+  dailyHours = dailyMinutes / 60;
+  dailyHours = Math.round(dailyHours * 10) / 10;
+  console.log('dailyMinutes: ', dailyMinutes);
+  console.log('dailyHours: ', dailyHours);
+  return dailyHours;
 }
 
-// jquery
 
+// jQuery
 $(document).ready(function() {
+    //Switch between
     $('.historyNavDaily').bind('touchstart click', function() {
       $('.dailyEntryContainer').css('display', 'inline');
       $('.weeklyEntryContainer').css('display', 'none');
